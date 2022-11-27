9fec988bcd273b0441e2d2f8f3c41851fc2acfc8
NabeelKM <nabeelkahlil403@gmail.com>
2022-11-27 10:45:46 +0700

    move env var to .env

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const nextConfig = {
   swcMinify: true,
   // images: {
   //   unoptimized: true
-  // },
-  env: {
-    DB_USERNAME: '<DB_USERNAME>',
-    DB_PASSWORD: '<DB_PASSWORD>',
-    DB_CONN_HOSTNAME: '<DB_HOST_CONN>',
-    DB_CONN_PORT: '<DB_HOST_PORT>',
-    DB_NAME: '<OWNER_USERNAME_IN_DB>',
-  }
+  // }
 }
 
 module.exports = nextConfig
