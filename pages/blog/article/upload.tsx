87e6e6de35343d17d0c6cf082df4988b0fd3fecd
NabeelKM <nabeelkahlil403@gmail.com>
2022-12-02 17:24:56 +0700

    add file uploader in client side

--- pages/blog/article/upload.tsx
+++ pages/blog/article/upload.tsx
@@ -11,10 +11,26 @@ const cvt = new Showdown.Converter({
   smoothLivePreview: true
 });
 
+class UploadFile {
+  imgUrl: string;
+  name: string;
+  file: File
+
+  constructor(file: File) {
+    this.file = file;
+    this.imgUrl = URL.createObjectURL(file)
+    this.name = file.name
+  }
+}
+
+const IMG_PATTERN = /!\[(.*)\]\((.*)\)/g;
+const LINK_PATTERN = /(?<=]\().*(?=\))/
+
 export default function UploadPage () {
   const [ textState, setTextState] = useState('');
   const [ tagInputState, setTagInputState] = useState('');
   const [ tagState, setTagState ] = useState<string[]>(['']);
+  const [ uploadFiles, setUploadFiles] = useState<UploadFile[]>([]);
 
   function handleTextChange(element: ChangeEvent<HTMLTextAreaElement>) {
     setTextState(element.target.value);
@@ -24,8 +40,24 @@ export default function UploadPage () {
     setTagInputState(element.target.value);
   }
 
+  function replaceImagePathForPreview(): string{
+    let newText = `${textState}`;
+    const imgMatches = textState.matchAll(IMG_PATTERN);
+  
+    for (let match of imgMatches) {
+      const imgPattern = match[0];
+      let imgurl = imgPattern.match(LINK_PATTERN);
+      const newimgurl = uploadFiles.find((uploadFile) => uploadFile.name == imgurl ? true : false)?.imgUrl
+      if (!newimgurl) continue;
+      const newImagePattern = imgPattern.replace(imgurl, newimgurl);
+      newText = newText.replace(imgPattern, newImagePattern)
+    }
+    return newText;
+  }
+
   function renderLivePreview() {
-    const htmlPreview = cvt.makeHtml(textState);
+    const mdText = replaceImagePathForPreview()
+    const htmlPreview = cvt.makeHtml(mdText);
     return parser(htmlPreview);
   }
 
@@ -99,6 +131,40 @@ export default function UploadPage () {
     }
   }
 
+  function filenameIsExist(filename: string): boolean {
+    const fileNames = uploadFiles.map(file => file.name);
+    if (fileNames.includes(filename)) return true;
+    return false;
+  }
+
+  async function handleFileUploadChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+
+    if (!files?.length) {
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++ ) {
+      const file = files.item(i);
+      if (file && !filenameIsExist(file.name)) {
+        const filename = file.name.replace(' ', '_')
+        const newUploadFile = new UploadFile(new File([file], filename, { type: 'Blob' }));
+        setUploadFiles([...uploadFiles, newUploadFile]);
+      }
+    }
+
+    event.target.value = '';
+  }
+
+  function renderFileList() {
+    return <div className={styles.fileListContiner}>
+      {uploadFiles.map(((files, index) => <div key={index}>
+          <img src={files.imgUrl}/>
+          <div>{files.name}</div>
+        </div>))}
+    </div>
+  }
+
   useEffect(() => {
     handleTagChange()
   }, [tagInputState])
@@ -118,7 +184,9 @@ export default function UploadPage () {
           </div>
 
           <div className={styles.editorArea}>
-            <div className={styles.form}>
+            <div className={styles.textEditor}>
+              {/* autocomplete */}
+              {/* https://ourcodeworld.com/articles/read/282/how-to-get-the-current-cursor-position-and-selection-within-a-text-input-or-textarea-in-javascript */}
               <textarea
                 id="live-editor"
                 value={textState}
@@ -126,8 +194,17 @@ export default function UploadPage () {
                 form="upload-form"
               />
             </div>
-            <div>
+            <div className={styles.liveRenderArea}>
               {renderLivePreview()}
+            </div>
+          </div>
+
+          <div>
+            <div className={styles.imageListArea}>
+              { uploadFiles ? renderFileList() : <></>}
+            </div>
+            <div className={styles.imageManager}>
+              <input type={'file'} onChange={handleFileUploadChange}/>
             </div>
           </div>
 
